372e178c101cde6671067c5b0a7fda30f1ba7aa2
Aflu <mohamedaflah186@gmail.com>
2024-06-09 15:02:53 +0530

    feat: treeshaking

--- src/_lib/utils/generateCSV.ts
+++ src/_lib/utils/generateCSV.ts
@@ -1,7 +1,7 @@
 import { Parser } from "json2csv";
 import * as fs from "fs";
 
-// Define the type for your JSON data
+
 interface Detail {
   name: string;
   mobile: string;
@@ -11,21 +11,17 @@ interface Detail {
   email: string;
 }
 
-// Sample JSON data
-// Fields to be included in the CSV
 const fields = ["name", "mobile", "company_name", "category", "visitCardUrl","email"];
 const opts = { fields };
 export const generateCSV = (jsonData: Detail[]) => {
   console.log("generating CSV");
   
   try {
-    // Create a new parser with the specified fields
+   
     const parser = new Parser<Detail>(opts);
 
-    // Convert JSON data to CSV
     const csv = parser.parse(jsonData);
 
-    // Write CSV data to a file
     fs.writeFileSync("output.csv", csv);
 
     console.log("CSV file has been created successfully.");
